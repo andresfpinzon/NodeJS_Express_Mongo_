60873476f316f70707bb2cbfe15d1bc950e21340
Andres <andrespinzonxd@gmail.com>
2024-08-28 11:08:23 -0500

    feat(back-end): add and test create Cursos

--- controllers/cursos.js
+++ controllers/cursos.js
@@ -2,8 +2,40 @@ const express = require('express');
 const Curso = require('../models/curso_model');
 const ruta = express.Router();
 
+
+// Función asíncrona para crear un objeto de tipo usuario
+async function crearCurso(body){
+    let curso = new Curso({
+        titulo  :body.titulo,
+        descripcion : body.descripcion,
+        alumnos     : body.alumnos,
+        calificacion : body.calificacion
+    })
+    return await curso.save();
+}
+
+
+/*
 ruta.get('/',(req,res)=>{
     res.json('respuesta a peticion GET de CURSOS funcionando correctamente...')
 });
+*/
+
+
+//Endpoint de tipo POST para el recurso CURSOS
+ruta.post('/', (req, res)=>{
+    let resultado = crearCurso(req.body);
+    resultado.then(curso=>{
+        res.json({
+            curso
+        })
+    }).catch(err =>{
+        res.status(400).json({
+            err
+        })
+    })
+});
+
+
 
 module.exports = ruta;
