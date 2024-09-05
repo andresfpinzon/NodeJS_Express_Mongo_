d6480894ac971b4d6686154de18669fdef0b8ecc
Andres <andrespinzonxd@gmail.com>
2024-09-05 11:35:41 -0500

    feat(back-end): add ssl to proyect

--- index.js
+++ index.js
@@ -10,6 +10,12 @@ const cursoRoutes = require('./routes/curso_routes');
 
 const express = require('express');
 const mongoose = require('mongoose');
+
+//Https
+const https = require('https')
+const fs = require('fs')
+const path = require('path')
+
 // Importa el paquete cors
 const cors = require('cors'); 
 
@@ -26,24 +32,32 @@ mongoose.connect(connectionString)
 
 // middleware
 const app = express();
+
+//Cargar el certificado SSL y la clave privada
+
+const options = {
+  key: fs.readFileSync(path.join(__dirname,'ssl', 'privatekey.pem')),
+  cert: fs.readFileSync(path.join(__dirname,'ssl', 'certificate.pem')),
+}
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 // Configuración básica de CORS (permite todas las solicitudes de cualquier origen)
-app.use(cors());
+//app.use(cors());
 
 // Opcional: configuración avanzada de CORS
-/* 
+
 const corsOptions = {
   origin: '*', // Reemplaza con el dominio que quieres permitir
-  methods: ['GET', 'POST', 'PUT', 'DELETE'], // Métodos permitidos
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'], // Métodos permitidos
   allowedHeaders: ['Content-Type', 'Authorization'], // Encabezados permitidos
-  credentials: true, // Permitir el envío de cookies
+  //credentials: true, // Permitir el envío de cookies
 };
 
 app.use(cors(corsOptions)); // Habilita cors con las opciones especificas
-//app.use('*', cors(corsOptions)) // habilida CORDS
-*/
+app.use('*', cors(corsOptions)) // habilita CORDS Pre-Fligth
+
 
 // Configuración de Swagger
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocs));
@@ -54,7 +68,15 @@ app.use('/api/cursos', cursoRoutes);
 
 
 const port = process.env.PORT || 3000;
+// servidor https
+https.createServer(options, app).listen(port, () => {
+  console.log('Api REST Ok, y ejecutándose...');
+  console.log(`Servidor HTTPS corriendo en  https://localhost:${port}`);
+});
+
+/*
+// servidor normal
 app.listen(port, () => {
     console.log('Api REST Ok, y ejecutándose...');
     console.log(`Usando la ruta http://localhost:${port}`);
-});
+});*/
